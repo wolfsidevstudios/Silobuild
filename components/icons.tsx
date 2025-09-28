abd6884eb5791a621522e4b8d0c3af7fa639259f
wolfsidevstudios <emartinezra112@icloud.com>
2025-09-28 14:23:47 -0600

    fix: Update Supabase logo to modern SVG
    Replaces the old static Supabase logo with a more modern, animated SVG. This improves visual consistency and potentially allows for future enhancements.

--- components/icons.tsx
+++ components/icons.tsx
@@ -86,9 +86,24 @@ export const GeminiLogo = ({ className = 'h-6' }) => (
 );
 
 export const SupabaseLogo = ({ className = 'h-6' }) => (
-    <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" className={className} fill="currentColor" color="#3ecf8e">
-        <title>Supabase</title>
-        <path d="M12.24 1.252a.24.24 0 0 0-.48 0L0 19.332v3.416h24v-3.416L12.24 1.252zM3.483 19.332l8.52-15.08 8.52 15.08H3.483z"/>
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 96 96" className={className}>
+      <title>Supabase</title>
+      <desc>
+        Supabase Icon Streamline Icon: https://streamlinehq.com
+      </desc>
+      <path fill="url(#supabase_icon_a)" d="M55.7223 93.4383c-2.4014 3.0241-7.2705 1.3672-7.3284-2.4942l-.846-56.4773h37.9752c6.8784 0 10.7146 7.9445 6.4375 13.3315l-36.2383 45.64Z"></path>
+      <path fill="url(#supabase_icon_b)" fillOpacity=".2" d="M55.7223 93.4383c-2.4014 3.0241-7.2705 1.3672-7.3284-2.4942l-.846-56.4773h37.9752c6.8784 0 10.7146 7.9445 6.4375 13.3315l-36.2383 45.64Z"></path>
+      <path fill="#3ecf8e" d="M40.278 2.56189c2.4014-3.024436 7.2705-1.36726 7.3284 2.49417l.3707 56.47724h-37.5c-6.87853 0-10.714819-7.9446-6.43753-13.3315L40.278 2.56189Z"></path>
+      <defs>
+        <linearGradient id="supabase_icon_a" x1="1011.58" x2="3189.12" y1="1286.71" y2="2199.97" gradientUnits="userSpaceOnUse">
+          <stop stopColor="#249361"></stop>
+          <stop offset="1" stopColor="#3ecf8e"></stop>
+        </linearGradient>
+        <linearGradient id="supabase_icon_b" x1="139.561" x2="1537.44" y1="-762.054" y2="1869.38" gradientUnits="userSpaceOnUse">
+          <stop></stop>
+          <stop offset="1" stopOpacity="0"></stop>
+        </linearGradient>
+      </defs>
     </svg>
 );
 
